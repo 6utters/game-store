2d8e065ebbe2e6c35998a63dde03786652c394e5
6utters <TheYomaLaughter@gmail.com>
2023-03-01 08:47:02 +0300

    project: jest init

--- client/src/pages/adminPage/index.ts
+++ client/src/pages/adminPage/index.ts
@@ -1,4 +1,4 @@
 export { default as AdminPage } from './ui/adminPage/AdminPage'
 export { default as AdminGenresPage } from './ui/adminGenresPage/AdminGenresPage'
-export {default as AdminFeaturesPage} from './ui/adminFeaturesPage/AdminFeaturesPage'
-export {default as AdminGamesPage} from './ui/adminGamesPage/AdminGamesPage'
+export { default as AdminFeaturesPage } from './ui/adminFeaturesPage/AdminFeaturesPage'
+export { default as AdminGamesPage } from './ui/adminGamesPage/AdminGamesPage'
---
+++ client/src/widgets/toolbar/ui/Toolbar.test.ts
@@ -0,0 +1,3 @@
+test('My first test', () => {
+	expect(Math.max(1, 4, 6)).toBe(6)
+})
